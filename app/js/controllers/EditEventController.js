7a336393a15d4bce4beec6a3f1dd4cdb5fdc04cc
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-01-25 08:56:47 +0100

    Summary Homework

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -3,9 +3,9 @@
 eventsApp.controller('EditEventController', EditEventController);
 
 function EditEventController($scope) {
+
     $scope.saveEvent = function (event, newEventForm)
     {
-        console.log(newEventForm);
         if(newEventForm.$valid)
         {
             window.alert('event ' + event.name + ' saved!');
