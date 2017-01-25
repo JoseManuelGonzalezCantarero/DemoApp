834f8c232927ae61d28a4cc807f99c8d09db8976
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-01-25 18:04:13 +0100

    Using Angular's $http Service

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,41 +1,16 @@
-eventsApp.factory('eventData', function () {
+eventsApp.factory('eventData', function ($http, $log) {
    return {
-        event: {
-            name: 'Angular Boot Camp',
-            date: 1359781015626,
-            time: '10:30 am',
-            location: {
-                address: 'Google Headquarters',
-                city: 'Mountain View',
-                province: 'CA'
-            },
-            imageUrl: '/img/angularjs-logo.png',
-            sessions: [
+        getEvent: function (successcb)
+        {
+            $http.get('data/event/1')
+                .success(function (data, status, headers, config)
                 {
-                    name: 'Directives Masterclass introductory',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!',
-                    upVoteCount: 0
-                },
+                    successcb(data);
+                })
+                .error(function (data, status, headers, config)
                 {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'John Doe',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it and how to get them to do it for you.',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Jane Doe',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
-                    upVoteCount: 0
-                }
-            ]
+                    $log.warn(data, status, headers(), config)
+                });
         }
    };
 });
