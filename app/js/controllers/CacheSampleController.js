e2c050dd10275d7373c6a0a51ce9d0de96ee6668
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-01-27 11:14:08 +0100

    Using Angular's $cacheFactory service

---
+++ app/js/controllers/CacheSampleController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+eventsApp.controller('CacheSampleController', CacheSampleController);
+
+function CacheSampleController($scope, myCache)
+{
+    $scope.addToCache = function (key, value)
+    {
+        myCache.put(key, value);
+    };
+
+    $scope.readFromCache = function (key)
+    {
+        return myCache.get(key);
+    };
+
+    $scope.getCacheStats = function ()
+    {
+        return myCache.info();
+    };
+}
