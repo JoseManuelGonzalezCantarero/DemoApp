f5e9b4b4b63f33eb8f4985450bebe87fc3eae711
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-02-08 18:23:04 +0100

    Using Angular's $route service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,13 +2,17 @@
 
 eventsApp.controller('EventController', EventController);
 
-function EventController($scope, eventData, $cookieStore, $routeParams)
+function EventController($scope, eventData, $cookieStore, $routeParams, $route)
 {
     $scope.sortorder = 'name';
     eventData.getEvent($routeParams.eventId)
         .$promise
         .then(function(event) {$scope.event = event;})
         .catch(function (response) { console.log(response);});
+
+    $scope.reload = function () {
+        $route.reload();
+    };
     
     $scope.upVoteSession = function (session) {
         if($cookieStore.get('upVote' + session.id) === undefined)
