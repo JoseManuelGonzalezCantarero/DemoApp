f9daed5ce3ac4f4194f32094fcf2e385767f1ee7
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-02-09 12:56:02 +0100

    Creating your first angular directive

--- app/js/app.js
+++ app/js/app.js
@@ -41,6 +41,12 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngCookies', 'ngRoute
                 template: '<h1>This is the about page</h1>'
             }
         );
+        $routeProvider.when('/sampleDirective',
+            {
+                templateUrl: 'templates/SampleDirective.html',
+                controller: 'SampleDirectiveController'
+            }
+        );
         $routeProvider.otherwise({redirectTo: '/events'});
 
         $locationProvider.html5Mode(true);
