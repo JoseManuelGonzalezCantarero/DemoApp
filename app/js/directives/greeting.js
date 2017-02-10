135f4385fe69f2783d5c3da631c1bed00cdb4d8a
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-02-10 16:05:25 +0100

    Directive priority and using terminal

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -9,6 +9,7 @@ function greeting()
     return {
         restrict: 'E',
         replace: true,
+        priority: -1,
         template: "<button class='btn' ng-click='sayHello()'>Say Hello</button>",
         controller: function ($scope) {
             var greetings = ['hello'];
@@ -26,6 +27,8 @@ function finnish()
 {
     return{
         restrict: 'A',
+        priority: -1,
+        terminal:true,
         require: 'greeting',
         link: function (scope, element, attrs, controller) {
             controller.addGreeting('hei');
@@ -37,6 +40,7 @@ function hindi()
 {
     return{
         restrict: 'A',
+        priority: -2,
         require: 'greeting',
         link: function (scope, element, attrs, controller) {
             controller.addGreeting('namasté');
