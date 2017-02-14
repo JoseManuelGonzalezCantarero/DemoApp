af6efb8045f605de0f24efea31dc6d7721a810f9
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-02-14 17:22:59 +0100

    Using Compile to transform the DOM

---
+++ app/js/directives/repeatx.js
@@ -0,0 +1,20 @@
+eventsApp.directive('repeatX', repeatX);
+
+function repeatX() {
+    return {
+        compile: function (element, attributes) {
+            for (var i = 0; i < Number(attributes.repeatX) - 1; i++)
+            {
+                element.after(element.clone().attr('repeat-x', 0));
+            }
+            return function (scope, element, attributes, controller) {
+                attributes.$observe('text', function (newValue) {
+                    if(newValue === 'Hello World')
+                    {
+                        element.css('color', 'red');
+                    }
+                });
+            }
+        }
+    };
+}
